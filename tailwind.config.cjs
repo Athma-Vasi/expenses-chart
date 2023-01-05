de340afd0617e351d131d7de9b4bb9aa6229ff12
Athma Chidambaravasi <athma.vasi@protonmail.com>
2023-01-04 20:02:29 -0700

    remove default boilerplate and add tailwind boilerplate

---
+++ tailwind.config.cjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        softRed: "hsl(10, 79%, 65%)",
+        myCyan: "hsl(186, 34%, 60%)",
+        darkBrown: "hsl(25, 47%, 15%)",
+        medBrown: "hsl(28, 10%, 53%)",
+        myCream: "hsl(27, 66%, 92%)",
+        paleOrange: "hsl(33, 100%, 98%)",
+      },
+    },
+    fontFamily: {
+      dmSans: ["DM Sans", "sans-serif"],
+    },
+    screens: {
+      sm: "375px",
+      md: "1440px",
+    },
+  },
+
+  plugins: [],
+};
